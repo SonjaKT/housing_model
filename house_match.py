bce58206fd86a6b1a9167a72135dcdde81d27886
Phil Nova <pnova8@gmail.com>
2015-06-06 14:01:24 -0700

    import *, like a n00b

--- house_match.py
+++ house_match.py
@@ -4,37 +4,7 @@
 
 import random
 
-class Renter(object):
-    def __init__(self, willingness_to_pay, ID):
-        self.ID = ID #integer representing order in an array
-        self.willingness_to_pay = willingness_to_pay
-        self.paying = 0
-        self.housing_preference_list = []
-        self.__matched = False
-
-    def get_matched(self):
-        return self.__matched
-
-    def set_matched(self, matched = True):
-        self.__matched = matched
-
-    def order_houses_test(self, house_list):
-        self.housing_preference_list = range(len(house_list))
-
-class House(object):
-    def __init__(self, niceness):
-        self.current_price = 100
-        self.niceness = niceness
-        self.__occupied = False
-        self.rented_by = None
-
-    def get_occupied(self):
-        return self.__occupied
-
-    def set_occupied(self, rented_by, occupied = True, auction_increment = 1.05):
-        self.current_price = self.current_price * auction_increment
-        self.rented_by = rented_by #integer ID of renter
-        self.__occupied = True
+from classes import *
 
 def stable_match(renters, houses, increment = 1.05):
     """Gale-Shapley stable matching algorithm.
