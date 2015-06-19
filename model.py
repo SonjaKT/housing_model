dc9849fa1c2b05b11f45a4e81fb19f8833391f16
Matt Lichti <mlichti@gmail.com>
2015-06-18 22:34:28 -0700

    added user prompts when running simulate.py in terminal for num of renters and housing units of each score, made model class based

--- model.py
+++ model.py
@@ -4,76 +4,49 @@ import math
 import numpy as np
 import house_match as h
 import csv
+import generators as g
+
+class HousingModel(object):
+
+    def __init__(self, renter_min_scores, house_scores):
+        self.houses = g.generate_houses(house_scores)
+        self.renters = g.generate_renters(renter_min_scores)
+        h.stable_match(self.renters, self.houses)
+
+    def mean_rent(self):
+        return np.mean([r.paying for r in self.renters if r.matched])
+
+    def median_rent(self):
+        return np.median([r.paying for r in self.renters if r.matched])
+
+    def renter_dict(self, dict_writer=None):
+        for renter in sorted(self.renters, key=lambda x: x.willingness_to_pay):
+            dic = {'max_rent': renter.willingness_to_pay,
+                   'min_score': renter.min_score, 'rent': renter.paying,
+                   'apt_score': (renter.renting.score if renter.matched else None)}
+            if dict_writer:
+                dict_writer.writerow(dic)
+            else:
+                print dic
+
+    def generate_csv(self, filename):
+        f = open(filename, 'w')
+        fieldnames = ['min_score', 'max_rent', 'rent', 'apt_score']
+        dw = csv.DictWriter(f, fieldnames)
+        dw.writeheader()
+        self.renter_dict(dict_writer=dw)
+        f.close()
 
 
-def generate_renters(n=1000, min_scores=[1, 2, 3, 4, 5]):
-    '''
-    Generates renters based on 2013 distribution of San Francisco household
-    incomes assuming each household can spend a max of 1/3 of pretax income
-    on rent.
-    inputs: n is number of renters, min_scores is the distribution of minimum
-    housing scores that renters will accept
-    '''
-    avg_income = 80000
-    sigma = .98  # sigma needed so 5% households above 423k (1.645 st dev)
-    renters = []
-    for i in xrange(n):
-        min_score = random.choice(min_scores)
-        income = int(np.random.lognormal(mean=math.log(avg_income),
-                                         sigma=sigma))
-        renters.append(classes.Renter(income/36, min_score))
-    return renters
-
-
-def generate_houses(house_scores={1: 150, 2: 200, 3: 250, 4: 200, 5: 150}):
-    '''
-    input the number houses of each score as a dictionary
-    '''
-    houses = []
-    for score, num in house_scores.iteritems():
-        houses += [classes.House(score) for h in xrange(num)]
-    return houses
-
-
-def simulate(num_renters=1500,
-             house_scores={1: 150, 2: 200, 3: 250, 4: 200, 5: 150},
+def test(renter_min_scores = {1: 30, 2: 30, 3: 30, 4: 30, 5: 30},
+             house_scores={1: 15, 2: 20, 3: 30, 4: 20, 5: 15}, 
              filename='output.csv'):
-    '''
-    runs simulation and outputs results as csv files
-    '''
-    f = open(filename, 'w')
-    fieldnames = ['min_score', 'max_rent', 'rent', 'apt_score']
-    dw = csv.DictWriter(f, fieldnames)
-    dw.writeheader()
-    houses = generate_houses(house_scores)
-    renters = generate_renters(num_renters)
-    h.stable_match(renters, houses)
-    for renter in sorted(renters, key=lambda x: x.willingness_to_pay):
-        dic = {'max_rent': renter.willingness_to_pay,
-               'min_score': renter.min_score, 'rent': renter.paying,
-               'apt_score': (renter.renting.score if renter.matched else None)}
-        # print dic
-        dw.writerow(dic)
-    f.close()
-    # print np.mean([r.paying for r in renters if r.matched])
-    # print np.median([r.paying for r in renters if r.matched])
-
-
-def run_simulation():
-    '''
-    runs simulator 3 times with 15,000 renters first with 10k houses with a
-    somewhat normal distribution of the 5 housing scores, then with
-    2k (luxury) score 5 houses added for a total of 12k houses, then with
-    12k houses with lower score houses added.
-    '''
-    renters = 15000
-    houses = {1: 1500, 2: 2000, 3: 2500, 4: 2000, 5: 1500}
-    simulate(num_renters=renters, house_scores=houses, filename='normal.csv')
-    houses[5] += 2000  # simulating with more luxury apartments
-    simulate(num_renters=renters, house_scores=houses, filename='luxury.csv')
-    houses = {1: 2000, 2: 2500, 3: 2500, 4: 2000, 5: 1500}
-    simulate(num_renters=renters, house_scores=houses, filename='cheap.csv')
-
+    
+    mod = HousingModel(renter_min_scores, house_scores)
+    print mod.mean_rent()
+    print mod.mean_rent()
+    mod.renter_dict()
+    mod.generate_csv(filename)
 
 if __name__ == '__main__':
-    run_simulation()
+    test()
---
+++ simulate.py
@@ -0,0 +1,39 @@
+import model as m
+
+def adding_luxury():
+    '''
+    runs model 2 times with 15,000 renters first with 9k houses with a
+    somewhat normal distribution of the 5 housing scores, then with
+    3k (luxury) score 5 houses added for a total of 12k houses
+    '''
+    renter_min_scores = {1: 3000, 2: 3000, 3: 3000, 4: 3000, 5: 3000}
+    house_scores = {1: 1000, 2: 2000, 3: 3000 , 4: 2000, 5: 1000}
+    mod = m.HousingModel(renter_min_scores, house_scores)
+    mod.generate_csv('normal.csv')
+    house_scores[5] += 3000  # simulating with more luxury apartments
+    lux_mod = m.HousingModel(renter_min_scores, house_scores)
+    lux_mod.generate_csv('more_luxury.csv')
+
+def user_input():
+    renters = {}
+    houses = {}
+    for dic, name in [renters, 'renters with minimum housing score'], \
+                     [houses, 'housing units with score']:
+        for score in [1,2,3,4,5]:
+            dic[score] = input('input number of %s %s: ' % (name, score))
+    while True:
+        mod = m.HousingModel(renters, houses)
+        if input('Type 1 to print renter info, or type 0 to output to CSV '):
+            mod.renter_dict()
+        else:
+            filename = input('enter filename in quotes ') 
+            mod.generate_csv(filename)
+        if not input('Type 1 to simulate building more housing, 0 to exit '):
+            break
+        for score in [1,2,3,4,5]:
+            houses[score] += input('%s units with score %s. # of units to add:'
+                                   % (houses[score], score))
+
+
+if __name__ == '__main__':
+    user_input()
